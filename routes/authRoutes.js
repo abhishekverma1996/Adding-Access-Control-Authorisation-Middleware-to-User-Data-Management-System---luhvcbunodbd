023b93ac6c51e416779a474256a2d622a36d62dd
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-05-14 20:39:34 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,11 +4,11 @@ const {
     login, signup, decodeToken
 } = require("../controllers/authControllers");
 
-
+const { grantAccessTo } = require("../middlewares/grantAccessTo");
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get('/decode', decodeToken)
+router.get('/decode', grantAccessTo(["user", "admin", "superadmin"]), decodeToken)
 
 module.exports = router;
